c55467aff0d01964911774df7f9d03b4b05b01c3
crz02 <skirfanahmed2206@gmail.com>
2025-09-24 18:03:51 +0530

    Update general.js

--- Scripts/general.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function () {
-    var preload = document.getElementById('preload');
-    preload.style.opacity = '0';
-    setTimeout(function () {
-        preload.style.display = 'none';
-    }, 500);
-});
